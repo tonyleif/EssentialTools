6abe7062834135015d38a197ac73624962f486f8
Anthony Leif <tonyleif@hotmail.com>
2017-09-15 22:52:40 -0500

    Page 160

--- EssentialTools.Tests/UnitTest2.cs
+++ EssentialTools.Tests/UnitTest2.cs
@@ -35,5 +35,38 @@ namespace EssentialTools.Tests
             //Assert.AreEqual(goalTotal, result);
             Assert.AreEqual(products.Sum(e => e.Price), result);
         }
+
+        private Product[] createProduct(decimal value)
+        {
+            return new [] { new Product { Price = value } };
+        }
+    
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentOutOfRangeException)]
+        public void Pass_Through_Variable_Discounts()
+        {
+            //arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0))).Throws<ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100))).Returns<decimal>(total => (total * .9M));
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive))).Returns<decimal>(total => total - 5);
+            var target = new LinqValueCalculator(mock.Object);
+
+            //act
+            decimal fiveDollarDiscount = target.ValueProducts(createProduct(5));
+            decimal tenDollarDiscount = target.ValueProducts(createProduct(10));
+            decimal fiftyDollarDiscount = target.ValueProducts(createProduct(50));
+            decimal hundredDollarDiscount = target.ValueProducts(createProduct(100));
+            decimal fiveHundredDollarDiscount = target.ValueProducts(createProduct(500));
+
+            //assert
+            Assert.AreEqual(5, fiveDollarDiscount, "$5 Fail");
+            Assert.AreEqual(5, tenDollarDiscount, "$10 Fail");
+            Assert.AreEqual(45, fiftyDollarDiscount, "$50 Fail");
+            Assert.AreEqual(90, hundredDollarDiscount, "$100 Fail");
+            Assert.AreEqual(450, fiveHundredDollarDiscount, "$500 Fail");
+            target.ValueProducts(createProduct(0));
+        }
     }
 }
